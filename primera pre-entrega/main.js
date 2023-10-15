e1adb6c7c9a0b090d10b562267f21012eff6f151
Agustina Elola <agustina.flor.elola@gmail.com>
2023-10-15 15:12:03 -0300

    Segunda Pre-Entrega

--- primera pre-entrega/main.js
+++ primera pre-entrega/main.js
@@ -1,51 +1,3 @@
-// alert("Bienvenido a Perla Estetica y Relax! A continuación ingrese su nombre y el servicio que se va a realizar para que se le sea asignado un turno...");
-
-
-// for(let turno = 10; turno <=20; turno++){
-
-//     if(turno <= 12){
-//         let nombre = prompt("Ingrese su nombre");
-//         while(nombre === ""){
-//             alert("Tiene que ingresar un nombre, de lo contrario no podremos asignarle un turno")
-
-//             nombre = prompt("Ingrese su nombre");
-//         }
-//         let servicio = prompt("Ingrese el servicio que se va a querer realizar (masajes, limpieza de cutis, dermaplaning o depilación láser");
-
-//         while(servicio !== "masajes" && servicio !== "limpieza de cutis" && servicio !== "dermaplaning" && servicio !== "depilación láser" && servicio !== "depilacion laser"){
-//             alert("Tiene que ingresar un servicio válido")
-//             servicio = prompt("Ingrese el servicio que se va a querer realizar (masajes, limpieza de cutis, dermaplaning o depilación láser");
-//         }
-//             alert(`Turno ${turno}am Nombre: ${nombre} para el servicio de ${servicio}`);
-
-
-//     }else{
-//         let nombre = prompt("Ingrese su nombre");
-
-//         while(nombre === ""){
-//             alert("Tiene que ingresar un nombre, de lo contrario no podremos asignarle un turno")
-
-//             nombre = prompt("Ingrese su nombre");
-//         }
-//         let servicio = prompt("Ingrese el servicio que se va a querer realizar (masajes, limpieza de cutis, dermaplaning o depilación láser");
-
-//         while(servicio !== "masajes" && servicio !== "limpieza de cutis" && servicio !== "dermaplaning" && servicio !== "depilación láser"){
-//             alert("Tiene que ingresar un servicio válido")
-//             servicio = prompt("Ingrese el servicio que se va a querer realizar (masajes, limpieza de cutis, dermaplaning o depilación láser");
-//         }
-            
-        
-//         alert(`Turno ${turno}pm Nombre: ${nombre} para el servicio de ${servicio}`);
-
-
-        
-//     }
-
-// }
-
-// alert("Desafortunadamente nos hemos quedado sin turnos para el día de hoy, lo esperamos mañana! Que tenga lindo día :)");
-
-
 alert("Bienvenido a Perla Estetica y Relax! A continuación, ingrese su nombre y el servicio que se va a realizar para que se le asigne un turno...");
 
 const turnos = [];
@@ -79,15 +31,15 @@ for (let turno = 10; turno <= 20; turno++) {
 
     alert(`Turno ${turnoInfo.turno} Nombre: ${turnoInfo.nombre} para el servicio de ${turnoInfo.servicio}`);
 
-}
+};
 
 alert("Desafortunadamente nos hemos quedado sin turnos para el día de hoy. ¡Lo esperamos mañana! Que tenga un lindo día :)");
 
 
+//Busqueda por nombre dentro de los turnos
 function buscarPorNombre(nombre) {
     return turnos.filter(turno => turno.nombre === nombre);
-}
-
+};
 
 const nombreBuscado = prompt("Ingrese el nombre que desea buscar:");
 const resultadosPorNombre = buscarPorNombre(nombreBuscado);
@@ -99,5 +51,5 @@ if (resultadosPorNombre.length > 0) {
         });
 } else {
     alert("No se encontraron resultados para el nombre ingresado.");
-}
+};
 
